0cf770352b2603a73be6fe16a3a733b5b82bcbd9
gchill <122580875+gchill@users.noreply.github.com>
2023-04-15 13:16:19 -0700

    Add files via upload

---
+++ collide.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class collide : MonoBehaviour {
+
+	public int lives;
+	public Text livetxt;
+	public GameObject player;
+	public Text gameovertext;
+
+      void Start(){
+		lives = 3;
+		livetxt.text = "lives = " + lives;
+		gameovertext.gameObject.SetActive(false);
+	}
+	
+	void txtChange(){
+		lives = lives - 1;
+		if(lives == 0){
+			Destroy(player);
+			gameovertext.gameObject.SetActive(true);
+		}
+		livetxt.text = "lives = " +lives;
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if(collision.gameObject.tag == "reward"){
+			Destroy(collision.gameObject);
+		}
+		if(collision.gameObject.tag == "enemy"){
+			txtChange();
+		}
+	}
+	
+}
---
+++ pause.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause : MonoBehaviour {
+	public GameObject PlayerController;
+	private bool Pause;
+	void Start () {
+		Pause = false;
+		PlayerController = GameObject.Find("Player Controller");
+	}
+	
+	
+	void Update () {
+		if(Input.GetButtonDown("pause")){
+			
+		}
+	}
+}
